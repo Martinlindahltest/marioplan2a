cc9a876316ed04c17c3cd18600814e0270296a81
Martinlindahltest <basmartin@gmail.com>
2019-05-06 15:28:29 +0200

    lektion 10

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -2,12 +2,13 @@ import React from 'react'
 
 export default function ProjectSummary() {
   return (
-    <div className="card z-depth-0 project-summary">
-    <div className="card-content grey-text text-darken-3">
-        <span className="card-title">Project title</span>
-        <p>Posted by Mnet ninja</p>
-        <p className="grey-text">3rd september, 2am</p>
+<div className="card z-depth-0 project-summary">
+      <div className="card-content grey-text text-darken-3">
+        <span className="card-title ">Project title</span>
+        <p>Posted by The Net Ninja</p>
+        <p className="grey-text">3rd September, 2am</p>
+      </div>
     </div>
-</div>
   )
 }
+
